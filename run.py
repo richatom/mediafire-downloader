d4f2e6954d36a994de2b4c993ca28906e07edf1d
Atom <bert.svensson42069@gmail.com>
2024-12-29 12:11:23 +0100

    Final update

--- run.py
+++ run.py
@@ -1,26 +1,17 @@
-import platform
 import subprocess
-import pyperclip
-import os
-import time
-def run():
-    print('Paste mediafire link:')
-    linkInput = input('')
 
-    print('Paste output folder:')
-    outputInput = input('')
-
-    argOutput = '-o ' + outputInput
-
-    subprocess.run('C:\\Windows\\System32\\cmd.exe', shell=True)
-    os.chdir('Documents')
-    time.sleep(0.2)
-    pyperclip.copy('python mediafire.py' + ' '+ linkInput + ' ' + argOutput)
-    pyperclip.paste()
+def main():
+    downloadable_link = input("Enter the downloadable link: ")
+    output_file_path = input("Enter the output file path: ")
+    command = f"python mediafire.py {downloadable_link} -o {output_file_path}"
+    
+    print(f"Running command: {command}")
+    try:
+        subprocess.run(command, shell=True, check=True)
+        print("Command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while running the command: {e}")
 
 
 if __name__ == "__main__":
-    try:
-        run()
-    except KeyboardInterrupt:
-        exit(0)
+    main()
